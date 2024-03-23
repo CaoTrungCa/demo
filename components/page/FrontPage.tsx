8358200f255cf03a42531c83a2af5dae103f5199
Cao Trung <trungcd@kctech.vn>
2024-03-24 01:36:27 +0700

    Revert "Revert "Update""
    This reverts commit b83194261de92bc218af418b70122f2c97d49abe.

---
+++ components/page/FrontPage.tsx
@@ -0,0 +1,7 @@
+import BannerBlock from "../block/BannerBlock";
+
+export default function FrontPage() {
+    return (
+        <BannerBlock front_page={true} path_banner='/banner/video_banner_home.mp4' />
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,6 +16,39 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          ".text-icon-theme": {
+            "color": "#ffffff",
+          },
+          ".bg-icon-theme": {
+            "background-color": "rgba(0, 0, 0, 0.4)",
+          },
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          ".text-icon-theme": {
+            "color": "#000000",
+          },
+          ".bg-icon-theme": {
+            "background-color": "rgba(255, 255, 255, 0.4)",
+          },
+        },
+      },
+    ],
+    styled: true,
+    base: true,
+    utils: true,
+    logs: false,
+    rtl: false,
+    prefix: '',
+    darkTheme: false,
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 }
 export default config
